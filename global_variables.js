8ea59d4d9ff035509fda8ceb88c0236f13bd5368
Nabeen-Thapa <nabeen2058@gmail.com>
2024-09-24 14:15:11 +0545

    Updated global variables

---
+++ global_variables.js
@@ -0,0 +1,9 @@
+// global variables
+// - store data, can accesiable from anywhere, no matter how deeeply nested
+
+//commonly used Global variables
+// _ _dirname: This variable stores the path to the current working directory.
+// _ _filename: This variable stores the path to the current working file.
+
+console.log(__dirname); //show curernt directory/folder 
+console.log(__filename); //show corrent file
