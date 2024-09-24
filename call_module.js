02abbd2e0e0a3bd75e824e451123af199f0fdb5d
Nabeen-Thapa <nabeen2058@gmail.com>
2024-09-24 15:14:13 +0545

    call sayhello module

---
+++ call_module.js
@@ -0,0 +1,9 @@
+const sayhello=require('./node_module.js'); //import module named node_module.js
+sayhello("Rohit");
+sayhello("Ram");
+sayhello("Shyam");
+
+// output:
+// hello Rohit
+// hello Ram
+// hello Shyam
---
+++ node_module.js
@@ -0,0 +1,12 @@
+// module
+// - reusable set/block of code that specific set of task or provides specific functionality
+// - contain variables, functions, classes, objects or any ther code that used for specific task
+// - help to organize into smaller, manageable piecesm
+// - can import any time to reusable the code without re-write code
+// - shared across multiple projects,
+// - let's do
+
+function sayhello(name){
+    console.log(`hello ${name}`);
+}
+module.exports =sayhello
