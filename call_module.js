cb0a5ad9085b45732b8344a134918dcfb7435375
Nabeen-Thapa <nabeen2058@gmail.com>
2024-09-24 21:49:11 +0545

    remove extra files

--- call_module.js
+++
@@ -1,9 +0,0 @@
-const sayhello=require('./node_module.js'); //import module named node_module.js
-sayhello("Rohit");
-sayhello("Ram");
-sayhello("Shyam");
-
-// output:
-// hello Rohit
-// hello Ram
-// hello Shyam
